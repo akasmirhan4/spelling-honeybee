7abfa54a86aa7a83bfd53026a814ad068846ef11
akasmirhan4 <amirrul.kasmirhan@gmail.com>
2024-05-20 23:01:19 +0800

    remove maingame component and add to layout and page instead

---
+++ src/app/play/layout.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import { WSJButton } from "../_components/wsj-button";
+import { LucideHome } from "lucide-react";
+import ShareButton from "../_components/ShareButton";
+import LeaderboardButton from "../_components/LeaderboardButton";
+
+export default function PlayLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <main className="flex max-h-screen w-screen flex-col items-center overflow-hidden bg-white text-black">
+      {/* topbar */}
+      <nav className="fixed top-0 z-50 flex h-12 w-full items-center justify-center border border-grey">
+        <div className="container flex h-full justify-between">
+          <div className="flex h-full flex-1 items-center">
+            <Link
+              href="/"
+              className="flex h-full items-center px-2 text-xs font-bold md:hover:bg-primary/50 ease-in-out transition-all active:bg-primary/50"
+            >
+              <LucideHome size={24} />
+            </Link>
+          </div>
+          <div className="flex h-full flex-1 items-center justify-end">
+            <ShareButton />
+            <LeaderboardButton />
+            <WSJButton />
+          </div>
+        </div>
+      </nav>
+      <div className="container mt-12">{children}</div>
+    </main>
+  );
+}
--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -1,28 +1,290 @@
-import Link from "next/link";
-import { MainGame } from "../_components/main-game";
-import { WSJButton } from "../_components/wsj-button";
+"use client";
 
-export default async function Play() {
+import { useEffect, useState } from "react";
+
+import { api } from "~/trpc/react";
+
+import toast from "react-hot-toast";
+import { useSearchParams } from "next/navigation";
+import type { GameData } from "~/types";
+
+import { RefreshCcw } from "lucide-react";
+import { Progress } from "../_components/progress";
+import { WordList } from "../_components/word-list";
+import { GameInput } from "../_components/game-input";
+import { LettersGrid } from "../_components/letters-grid";
+import Confetti from "../_components/Confetti";
+
+// TODO:
+// - shake animation when word is not valid
+// - add notification
+// - disable submitting words if textinput is empty or api is loading
+
+export default function PlayPage() {
+  const [textInput, setTextInput] = useState("");
+  const amirrul = api.game.getGameData.useMutation();
+  const wsj = api.game.getWSJGameData.useMutation();
+  const [validLetters, setValidLetters] = useState<string[]>([]);
+  const [centerLetter, setCenterLetter] = useState("");
+  const [outerLetters, setOuterLetters] = useState<string[]>([]);
+  const [answers, setAnswers] = useState<string[]>([]);
+  const [submittedWords, setSubmittedWords] = useState<string[]>([]);
+  const [isConfettiVisible, setIsConfettiVisible] = useState(false);
+
+  const playWSJ = useSearchParams().get("wsj") === "true";
+
+  useEffect(() => {
+    setSubmittedWords([]);
+    const date = new Date().toLocaleDateString();
+    const displayDate = localStorage.getItem("displayDate");
+    if (!displayDate) {
+      localStorage.setItem("displayDate", date);
+    } else {
+      if (displayDate !== date) {
+        localStorage.setItem("displayDate", date);
+        localStorage.setItem("wsjSubmittedWords" + date, "");
+        localStorage.setItem("amirrulSubmittedWords" + date, "");
+      }
+    }
+    if (playWSJ) {
+      wsj.mutate(
+        {},
+        {
+          onSuccess: (data: GameData) => {
+            setOuterLetters(data.outerLetters);
+            setCenterLetter(data.centerLetter);
+            setValidLetters(data.validLetters);
+            setAnswers(data.answers);
+          },
+        },
+      );
+      const wsjSubmittedWords = localStorage.getItem(
+        "wsjSubmittedWords" + date,
+      );
+      if (wsjSubmittedWords) {
+        setSubmittedWords(wsjSubmittedWords.split(","));
+      }
+    } else {
+      amirrul.mutate(
+        {},
+        {
+          onSuccess: (data: GameData) => {
+            setOuterLetters(data.outerLetters);
+            setCenterLetter(data.centerLetter);
+            setValidLetters(data.validLetters);
+            setAnswers(data.answers);
+          },
+        },
+      );
+      const amirrulSubmittedWords = localStorage.getItem(
+        "amirrulSubmittedWords" + date,
+      );
+      if (amirrulSubmittedWords) {
+        setSubmittedWords(amirrulSubmittedWords.split(","));
+      }
+    }
+  }, [playWSJ]);
+
+  // if textinput is longer than 19, alert window
+  useEffect(() => {
+    if (textInput.length >= 19) {
+      toast.error("You have reached the maximum number of characters");
+      setTextInput("");
+    }
+  }, [textInput]);
+
+  const onSubmitWord = async () => {
+    if (!textInput) return;
+    const date = new Date().toLocaleDateString();
+
+    const errors = [];
+
+    const _textInput = textInput.toUpperCase();
+
+    if (_textInput.length < 4) {
+      const error = "Word must be at least 4 characters long";
+      errors.push(error);
+      toast.error(error);
+    }
+
+    if (!_textInput.includes(centerLetter)) {
+      const error = "Word must contain the center letter";
+      errors.push(error);
+      toast.error(error);
+    }
+
+    if (submittedWords.includes(_textInput)) {
+      const error = `You have already submitted "${_textInput}"!`;
+      errors.push(error);
+      toast.error(error);
+    }
+
+    // check if word contain other letters
+    if (
+      !_textInput.split("").every((letter) => validLetters.includes(letter))
+    ) {
+      const error = `${_textInput} contains invalid letters!`;
+      errors.push(error);
+      toast.error(error);
+    }
+
+    // check if word is valid
+    if (errors.length == 0) {
+      if (!answers.includes(_textInput.toLowerCase())) {
+        const error = `"${_textInput}" is not a valid word!`;
+        errors.push(error);
+        toast.error(error);
+      } else {
+        const _submittedWords = [...submittedWords, _textInput];
+        setSubmittedWords(_submittedWords);
+        if (playWSJ) {
+          localStorage.setItem(
+            "wsjSubmittedWords" + date,
+            _submittedWords.join(","),
+          );
+        } else {
+          localStorage.setItem(
+            "amirrulSubmittedWords" + date,
+            _submittedWords.join(","),
+          );
+        }
+
+        // check if word is pangram
+        if (new Set(_textInput.split("")).size == 7) {
+          toast.success("Pangram!");
+          setIsConfettiVisible(true);
+        } else {
+          toast.success(`"${_textInput}" is a valid word!`);
+        }
+      }
+    }
+    setTextInput("");
+  };
+
+  return (!playWSJ && !amirrul.isPending) || (playWSJ && !wsj.isPending) ? (
+    <div className="flex flex-col justify-center md:container md:flex-row-reverse">
+      <div className="flex w-screen flex-col md:w-1/2 md:flex-1">
+        <Progress words={submittedWords} answers={answers} />
+        {/* word list */}
+        <WordList words={submittedWords} />
+      </div>
+      <div className="flex w-screen flex-1 flex-col items-center md:w-1/2">
+        <GameInput
+          textInput={textInput}
+          onTextInput={setTextInput}
+          specialLetter={centerLetter}
+          usableLetter={outerLetters}
+          onSubmitWord={onSubmitWord}
+        />
+        <LettersGrid
+          specialLetter={centerLetter}
+          usableLetter={outerLetters}
+          onLetterClick={(letter) => setTextInput(textInput + letter)}
+        />
+        <div className="mb-8 flex gap-6">
+          <CustomButton
+            text="Delete"
+            onClick={() => {
+              setTextInput(textInput.slice(0, -1));
+            }}
+          />
+          <CustomIconButton
+            iconNode={<RefreshCcw className="size-6" />}
+            onClick={() => {
+              const shuffled = [...outerLetters].sort(
+                () => Math.random() - 0.5,
+              );
+              setOuterLetters(shuffled);
+            }}
+          />
+          <CustomButton text="Enter" onClick={onSubmitWord} />
+        </div>
+        <Confetti
+          active={isConfettiVisible}
+          onComplete={() => setIsConfettiVisible(false)}
+        />
+      </div>
+    </div>
+  ) : (
+    <Loading />
+  );
+}
+
+type CustomButtonProps = {
+  text?: string;
+  onClick?: () => void;
+};
+function CustomButton({ text, onClick }: CustomButtonProps) {
+  const [onMouseDown, setOnMouseDown] = useState(false);
+
   return (
-    <main className="flex max-h-screen w-screen flex-col items-center overflow-hidden bg-white text-black">
-      {/* topbar */}
-      <div className="border-grey fixed top-0 z-50 flex h-12 w-full items-center justify-center border">
-        <div className="container flex justify-between h-full">
-          <div className="flex-1" />
-          <div className="flex h-full flex-1 items-center justify-end">
-            <WSJButton />
-            <Link
-              href="/"
-              className="flex h-full items-center px-6 text-xs font-bold hover:bg-primary/50"
-            >
-              <h1>Home</h1>
-            </Link>
+    <div
+      className={`text-md flex cursor-pointer select-none items-center rounded-full border border-grey px-8 md:px-10 ${onMouseDown ? "bg-grey/50" : "bg-transparent"}`}
+      onMouseDown={() => {
+        setOnMouseDown(true);
+        onClick && onClick();
+      }}
+      onMouseUp={() => setOnMouseDown(false)}
+      onMouseLeave={() => setOnMouseDown(false)}
+      onTouchStart={() => setOnMouseDown(true)}
+      onTouchEnd={() => setOnMouseDown(false)}
+    >
+      {text}
+    </div>
+  );
+}
+
+type CustomIconButtonProps = {
+  iconNode: React.ReactNode;
+  onClick?: () => void;
+};
+function CustomIconButton({ iconNode, onClick }: CustomIconButtonProps) {
+  const [onMouseDown, setOnMouseDown] = useState(false);
+
+  return (
+    <div
+      className={`border-gray flex cursor-pointer select-none items-center rounded-full border p-2 ${onMouseDown ? "bg-grey/50" : "bg-transparent"}`}
+      onMouseDown={() => {
+        setOnMouseDown(true);
+        onClick && onClick();
+      }}
+      onMouseUp={() => setOnMouseDown(false)}
+      onMouseLeave={() => setOnMouseDown(false)}
+      onTouchStart={() => setOnMouseDown(true)}
+      onTouchEnd={() => setOnMouseDown(false)}
+    >
+      {iconNode}
+    </div>
+  );
+}
+
+function Loading() {
+  return (
+    <div className="my-8 flex flex-col items-center justify-center">
+      <svg
+        aria-hidden="true"
+        className="dark:primary/50 size-12 animate-spin fill-primary text-gray-200"
+        viewBox="0 0 100 101"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+          fill="currentColor"
+        />
+        <path
+          d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+          fill="currentFill"
+        />
+      </svg>
+      <div>
+        {/* Make a loading text that load one letter at a time */}
+        <div className="inline-block">
+          <div className="animate-type mt-4 overflow-hidden whitespace-nowrap text-lg font-bold text-gray-400">
+            Loading...
           </div>
         </div>
       </div>
-      <div className="container mt-12">
-        <MainGame />
-      </div>
-    </main>
+    </div>
   );
 }
