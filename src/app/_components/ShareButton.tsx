656d363a58f575f66e166aced9eae9f2fa5c2170
akasmirhan4 <amirrul.kasmirhan@gmail.com>
2024-05-20 23:06:16 +0800

    made copy mockup awaited for promise

--- src/app/_components/ShareButton.tsx
+++ src/app/_components/ShareButton.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { LucideShare2 } from "lucide-react";
 import toast from "react-hot-toast";
@@ -6,12 +6,15 @@ import toast from "react-hot-toast";
 export default function ShareButton({}) {
   return (
     <button
-      className="flex h-full items-center px-2 text-xs font-bold md:hover:bg-primary/50 active:bg-primary/50 ease-in-out transition-all"
-      onClick={() => {
-        navigator.clipboard.writeText("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
-        toast.success("Results copied!", {
-            position: "bottom-center",
-        })
+      className="flex h-full items-center px-2 text-xs font-bold transition-all ease-in-out active:bg-primary/50 md:hover:bg-primary/50"
+      onClick={async () => {
+        await navigator.clipboard
+          .writeText("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+          .then(() => {
+            toast.success("Results copied!", {
+              position: "bottom-center",
+            });
+          });
       }}
     >
       <LucideShare2 size={24} />
