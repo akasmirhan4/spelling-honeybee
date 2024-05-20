67510d92e49d60224b10f81baed08d03b11f6135
akasmirhan4 <amirrul.kasmirhan@gmail.com>
2024-05-21 06:50:15 +0800

    feat: Add background color to topbar

--- src/app/play/layout.tsx
+++ src/app/play/layout.tsx
@@ -13,7 +13,7 @@ export default function PlayLayout({
   return (
     <main className="flex max-h-screen w-screen flex-col items-center overflow-hidden bg-white text-black">
       {/* topbar */}
-      <nav className="fixed top-0 z-50 flex h-12 w-full items-center justify-center border border-gray-300">
+      <nav className="fixed top-0 z-50 flex h-12 w-full items-center justify-center border border-gray-300 bg-white">
         <div className="container flex h-full justify-between">
           <div className="flex h-full flex-1 items-center gap-2">
             <Button asChild variant="ghost">
