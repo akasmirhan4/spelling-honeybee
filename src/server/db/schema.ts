d52cd5931e47c31343014d29ea18cb37d6096409
akasmirhan4 <amirrul.kasmirhan@gmail.com>
2024-05-25 22:37:48 +0800

    config drizzle, vercel pg

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,34 +1,53 @@
-// Example model schema from the Drizzle docs
-// https://orm.drizzle.team/docs/sql-schema-declaration
-
-import { sql } from "drizzle-orm";
 import {
-  index,
-  pgTableCreator,
   serial,
+  pgTableCreator,
   timestamp,
   varchar,
+  index,
+  integer,
+  pgEnum,
+  boolean,
 } from "drizzle-orm/pg-core";
 
-/**
- * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
- * database instance for multiple projects.
- *
- * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
- */
-export const createTable = pgTableCreator((name) => `spelling-honeybee_${name}`);
+export const createTable = pgTableCreator(
+  (name) => `spelling_honeybee_${name}`,
+);
 
-export const posts = createTable(
-  "post",
+export const ScoreRankName = [
+  "Genius",
+  "Amazing",
+  "Great",
+  "Nice",
+  "Solid",
+  "Good",
+  "Moving Up",
+  "Good Start",
+  "Beginner",
+] as const;
+
+export type ScoreRankNameType = typeof ScoreRankName[number];
+
+export const rankEnum = pgEnum("rank", ScoreRankName);
+export const gameVersionEnum = pgEnum("game_version", ["NYT", "AK"]);
+
+export const userScore = createTable(
+  "user_score",
   {
     id: serial("id").primaryKey(),
-    name: varchar("name", { length: 256 }),
-    createdAt: timestamp("created_at", { withTimezone: true })
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: timestamp("updatedAt", { withTimezone: true }),
+    username: varchar("username", {
+      length: 10,
+    }).notNull(),
+    score: integer("score").notNull(),
+    rank: rankEnum("rank").notNull(),
+    gameVersion: gameVersionEnum("game_version").notNull(),
+    nSubmittedWords: integer("n_submitted_words").notNull(),
+    pangramFound: boolean("pangram_found").notNull(),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updatedAt"),
   },
-  (example) => ({
-    nameIndex: index("name_idx").on(example.name),
-  })
+  (table) => {
+    return {
+      nameIndex: index("name_index").on(table.username),
+    };
+  },
 );
